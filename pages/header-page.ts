85d47bb2e896e5701b42239b9574cf53bbf1aa59
Michał Siedlecki <michal.siedlecki@intive.com>
2023-10-06 23:07:06 +0200

    fix mobile

--- pages/header-page.ts
+++ pages/header-page.ts
@@ -2,6 +2,7 @@ import { Page } from '@playwright/test';
 
 export class HeaderPage {
   readonly searchInput = this.page.locator('#search-desktop');
+  readonly mobileSearchInput = this.page.locator('#search-mobile');
 
   constructor(public readonly page: Page) {}
 }
